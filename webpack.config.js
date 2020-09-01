9f421e537e1fdd2872c507732ea8494033dba358
unknown <emmanueldmlr@gmail.com>
2020-09-01 14:29:22 +0100

    updateddist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 
 module.exports = {
   entry: "./app/index.js",
@@ -18,8 +20,15 @@ module.exports = {
   plugins:[
       new HtmlWebpackPlugin({
           template:'./app/index.html'
-      })
+      }),
       
+      new CopyPlugin(
+          {
+            patterns:[
+              {from:'_redirects'}
+            ]
+          }
+       )
   ],
   devServer:{
     historyApiFallback: true
